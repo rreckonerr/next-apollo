4cc24433b54734db867390a6f729adf0ddde2562
volodymyrradchenko <bratko_bob@yahoo.com>
2019-01-13 16:31:08 +0200

    add test dependencies

---
+++ styles/utils/hex2Rgba.test.js
@@ -0,0 +1,9 @@
+describe('sample test 101', () => {
+    it('works as expected', () => {
+        expect(1).toEqual(1);
+    });
+    it('test the range', () => {
+        const age = 200;
+        expect(age).toBeGreaterThan(100);
+    })
+})
