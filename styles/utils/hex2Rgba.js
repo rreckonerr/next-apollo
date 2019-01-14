7fe528bb23843d41ef0f28309b1452572c07ddbf
volodymyrradchenko <bratko_bob@yahoo.com>
2019-01-14 10:21:12 +0200

    refactor hex2Rgba; add hex2Rgba test

--- styles/utils/hex2Rgba.js
+++ styles/utils/hex2Rgba.js
@@ -1,6 +1,64 @@
-export function hex2Rgba(hex, alpha = 1) {
-    const r = parseInt(hex.substring(1, 3), 16);
-    const g = parseInt(hex.substring(3, 5), 16);
-    const b = parseInt(hex.substring(5, 7), 16);
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
-  }
+'use strict'
+/**
+ * Constants.
+ */
+var BASE = 16;
+var HEX_REGEX = /^#?[a-fA-F0-9]+$/;
+var HEX_SHORTHAND_LENGTH = 3;
+var HEX_LENGTH = 6;
+
+/**
+ * Converts hexadecimal to RGBA.
+ *
+ * @param  {String}        hex     - The hexadecimal.
+ * @param  {Number|String} [alpha] - The alpha transparency.
+ * @return {String}                - The RGBA.
+ */
+export function hex2Rgba(hex, alpha) {
+    if (!HEX_REGEX.test(hex)) {
+        throw Error('hex2rgba: first argument has invalid hexadecimal characters');
+    }
+
+    // trim unnecessary characters
+    if (hex[0] === '#') {
+        hex = hex.slice(1);
+    }
+
+    // expand shorthand
+    if (hex.length === HEX_SHORTHAND_LENGTH) {
+        hex = hex.split('');
+        hex.splice(2, 0, hex[2]);
+        hex.splice(1, 0, hex[1]);
+        hex.splice(0, 0, hex[0]);
+        hex = hex.join('');
+    }
+
+    if (hex.length !== HEX_LENGTH) {
+        throw Error('hex2rgba: first argument has invalid hexadecimal length');
+    }
+
+    // convert hex to rgb
+    var values = [
+        parseInt(hex.slice(0, 2), BASE),
+        parseInt(hex.slice(2, 4), BASE),
+        parseInt(hex.slice(4, 6), BASE)
+    ];
+
+    alpha = typeof alpha === 'number' ? alpha : parseFloat(alpha);
+    if (alpha >= 0 && alpha <= 1) {
+        values.push(alpha);
+    } else {
+        values.push(1);
+    }
+
+    return 'rgba(' + values.join(',') + ')';
+};
+
+
+
+// export function hex2Rgba(hex, alpha = 1) {
+//     const r = parseInt(hex.substring(1, 3), 16);
+//     const g = parseInt(hex.substring(3, 5), 16);
+//     const b = parseInt(hex.substring(5, 7), 16);
+//     return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+//   }
--- styles/utils/hex2Rgba.test.js
+++ styles/utils/hex2Rgba.test.js
@@ -1,9 +1,90 @@
-describe('sample test 101', () => {
-    it('works as expected', () => {
-        expect(1).toEqual(1);
-    });
-    it('test the range', () => {
-        const age = 200;
-        expect(age).toBeGreaterThan(100);
-    })
-})
+/**
+ * Module dependencies.
+ */
+import { hex2Rgba } from "./hex2Rgba";
+
+/**
+ * Tests.
+ */
+describe("hex2Rgba", () => {
+  it("throws error if first argument is invalid", () => {
+    const badArgHex2Rgba = () => {
+      [
+        undefined,
+        null,
+        0,
+        false,
+        Object,
+        Function,
+        "",
+        "xxx",
+        "gggggg",
+        "#zzz",
+        "#oooooo"
+      ].forEach(value => {
+        hex2Rgba(value);
+      });
+    };
+    expect(badArgHex2Rgba).toThrowError(
+      "hex2rgba: first argument has invalid hexadecimal characters"
+    );
+  });
+
+  it("throws error if hexadecimal length is invalid", () => {
+    const badLengthHex2Rgba = () => {
+      [
+        "b",
+        "b0",
+        "b000",
+        "b000",
+        "b0000",
+        "b000000",
+        "#b",
+        "#b0",
+        "#b000",
+        "#b0000",
+        "#b000000"
+      ].forEach(value => {
+        hex2Rgba(value);
+      });
+      expect(badLengthHex2Rgba).toThrowError(
+        "hex2rgba: first argument has invalid hexadecimal length"
+      );
+    };
+  });
+
+  it("converts shorthand hexadecimal", () => {
+    expect(hex2Rgba("FB1")).toEqual("rgba(255,187,17,1)");
+    expect(hex2Rgba("1CE")).toEqual("rgba(17,204,238,1)");
+  });
+
+  it("converts hexadecimal without `#`", () => {
+    expect(hex2Rgba("bada55")).toEqual("rgba(186,218,85,1)");
+    expect(hex2Rgba("A55")).toEqual("rgba(170,85,85,1)");
+  });
+
+  it("converts hexadecimal with `#`", () => {
+    expect(hex2Rgba("#bada55")).toEqual("rgba(186,218,85,1)");
+    expect(hex2Rgba("#A55")).toEqual("rgba(170,85,85,1)");
+  });
+
+  it("converts case-insensitive hexadecimal", () => {
+    expect(hex2Rgba("C0FFEE")).toEqual("rgba(192,255,238,1)");
+    expect(hex2Rgba("c0fFEe")).toEqual("rgba(192,255,238,1)");
+    expect(hex2Rgba("facade")).toEqual("rgba(250,202,222,1)");
+    expect(hex2Rgba("Facade")).toEqual("rgba(250,202,222,1)");
+    expect(hex2Rgba("FACADE")).toEqual("rgba(250,202,222,1)");
+  });
+
+  it("overrides default alpha value if valid", () => {
+    expect(hex2Rgba("f00", 0.42)).toEqual("rgba(255,0,0,0.42)");
+    expect(hex2Rgba("ff0000", "0")).toEqual("rgba(255,0,0,0)");
+    expect(hex2Rgba("#f00", 1)).toEqual("rgba(255,0,0,1)");
+  });
+
+  it("does not override default alpha value if invalid", () => {
+    expect(hex2Rgba("f00", "zero")).toEqual("rgba(255,0,0,1)");
+    expect(hex2Rgba("f00", -1)).toEqual("rgba(255,0,0,1)");
+    expect(hex2Rgba("f00", 1.1)).toEqual("rgba(255,0,0,1)");
+  });
+});
